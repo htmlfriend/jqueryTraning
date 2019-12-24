3321b08970aca57997933fcf79e1647fbe804413
Yurii <u181@bigmir.net>
2019-12-23 22:35:42 -0500

    train a slider for page

--- lesson 1/js/main.js
+++ lesson 1/js/main.js
@@ -144,4 +144,76 @@ $(document).ready(function() {
   //     e.preventDefault();
   //     $(this).css("border", "1px solid red");
   //   });
+  var slideNow = 1;
+  var slideCount = $(".sliderwprapper").children().length;
+  var navBtn = $(".slide-nav");
+  var slideTime = 2000;
+  $("body").keypress(function(e) {
+    console.log(e.keyCode);
+    if (e.keyCode == 44) {
+      prevSlide();
+    } else if (e.keyCode == 46) {
+      nextSlide();
+    } else {
+      //   setInterval(nextSlide, slideTime);
+    }
+  });
+
+  navBtn.click(function() {
+    navBtn = $(this).index();
+    $(".active").removeClass("active");
+    $(this).toggleClass("active");
+    if (navBtn + 1 != slideNow) {
+      var translateWidth = -$(".viewport").width() * navBtn;
+      $(".sliderwprapper").css({
+        transform: "translate(" + translateWidth + "px,0)",
+        "-o-transform": "translate(" + translateWidth + "px,0)",
+        "-webkit-transform": "translate(" + translateWidth + "px,0)"
+      });
+      slideNow = navBtn + 1;
+    }
+  });
+
+  $(".btn-next").click(nextSlide);
+
+  $(".btn-prev").click(prevSlide);
+  function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+      $(".sliderwprapper").css({
+        transform: "translate(0,0)",
+        "-o-transform": "translate(0,0)",
+        "-webkit-transform": "translate(0,0)"
+      });
+      slideNow = 1;
+    } else {
+      var translateWidth = -$(".viewport").width() * slideNow;
+      $(".sliderwprapper").css({
+        transform: "translate(" + translateWidth + "px,0)",
+        "-o-transform": "translate(" + translateWidth + "px,0)",
+        "-webkit-transform": "translate(" + translateWidth + "px,0)"
+      });
+      slideNow++;
+    }
+  }
+
+  function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+      var translateWidth = -$(".viewport").width() * (slideCount - 1);
+
+      $(".sliderwprapper").css({
+        transform: "translate(" + translateWidth + "px,0)",
+        "-o-transform": "translate(" + translateWidth + "px,0)",
+        "-webkit-transform": "translate(" + translateWidth + "px,0)"
+      });
+      slideNow = slideCount;
+    } else {
+      var translateWidth = -$(".viewport").width() * (slideNow - 2);
+      $(".sliderwprapper").css({
+        transform: "translate(" + translateWidth + "px,0)",
+        "-o-transform": "translate(" + translateWidth + "px,0)",
+        "-webkit-transform": "translate(" + translateWidth + "px,0)"
+      });
+      slideNow--;
+    }
+  }
 });
